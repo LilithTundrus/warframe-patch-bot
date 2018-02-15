0f9dfe7ce4e85649f5d763783c7aee4b861efca5
Lilith Tundrus <echofox123@gmail.com>
2018-02-15 18:15:58 -0500

    Getting the basics working

--- bot.js
+++ bot.js
@@ -5,8 +5,8 @@ let Discord = require('discord.io');                                // discord A
 let fs = require('fs');                                             // used to read helpNotes.txt
 let os = require('os');                                             // os info lib built into node
 let Logger = require('./lib/loggerClass');
+let scraper = require('./lib/scraper');
 let logger = new Logger;
-
 
 let bot = new Discord.Client({                                      // Initialize Discord Bot with config.token
     token: config.token,
@@ -33,3 +33,40 @@ bot.on('disconnect', function (evt) {
         logger.error('Reconnect failed...');
     }
 });
+
+bot.on('message', function (user, userID, channelID, message, evt) {
+    if (message.substring(0, 1) == config.commandCharDefault) {                           // listen for messages that will start with `~`
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+        // log any messages sent to the bot for debugging
+        fs.appendFileSync('discordMessagelog.log', `${user} sent: ${message} at ${Date.now()}`);
+        console.log(`${user} sent: ${message} at ${Date.now()}`);
+        args = args.splice(1);
+        switch (cmd) {                                              // bot needs to know if it will execute a command
+            case 'help':                                            // display the help file
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'PlaceHolder'
+                });
+                break;
+            // Eventually format this to be pretty and show a LOT more stats
+            case 'ver':
+                bot.sendMessage({
+                    to: channelID,
+                    message: `Version: ${ver} Running on server: ${os.type()} ${os.hostname()} ${os.platform()} ${os.cpus()[0].model}`
+                });
+                break;
+            case 'scrape':
+                return scraper.test()
+                    .then((message) => {
+                        bot.sendMessage({
+                            to: channelID,
+                            message: message
+                        });
+                    })
+        }
+    }
+});
+
+
+// Put the scheduler here!
