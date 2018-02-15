df11c7a29a6a4fffc9baeaf761e45c0475b73cab
Lilith Tundrus <echofox123@gmail.com>
2018-02-14 23:11:47 -0500

    Installed chalk, added basic bot functionality

--- bot.js
+++ bot.js
@@ -1,21 +1,35 @@
 'use strict';                                                       // more stringent error reporting for small things
-const config = require('./config.js');                              // conifg/auth data
+const config = require('./config/config.js');                              // conifg/auth data
 const ver = config.botVersion;
-var Discord = require('discord.io');                                // discord API wrapper
-var fs = require('fs');                                             // used to read helpNotes.txt
-var os = require('os');                                             // os info lib built into node
+let Discord = require('discord.io');                                // discord API wrapper
+let fs = require('fs');                                             // used to read helpNotes.txt
+let os = require('os');                                             // os info lib built into node
+let Logger = require('./lib/loggerClass');
+let logger = new Logger;
 
-var bot = new Discord.Client({                                      // Initialize Discord Bot with config.token
+
+let bot = new Discord.Client({                                      // Initialize Discord Bot with config.token
     token: config.token,
     autorun: true
 });
 
-console.log('Attempting to connect to Discord')
+logger.debug('Attempting to connect to Discord');
 bot.on('ready', function (evt) {                                    // do some logging and start ensure bot is running
-    console.log('Connected to Discord...');
-    console.log(`Logged in as: ${bot.username} - (${bot.id})`);
+    logger.info('Connected to Discord...');
+    logger.info(`Logged in as: ${bot.username} - (${bot.id})`);
     bot.setPresence({                                               // make the bot 'play' soemthing
         idle_since: null,
         game: { name: 'Debug mode' }
     });
+});
+
+bot.on('disconnect', function (evt) {
+    logger.warn(`Bot DISCONNECTED at ${new Date().toISOString()}`);
+    logger.debug('Attempting reconnect...');
+    bot.connect();
+    if (bot.connected == true) {
+        logger.info('Reconnected to Discord!');
+    } else {
+        logger.error('Reconnect failed...');
+    }
 });
