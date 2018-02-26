44dec708ed33cb1511369851b4149572c3ad776d
Lilith Tundrus <echofox123@gmail.com>
2018-02-26 18:41:41 -0500

    creating a class to encompass this project (reduce per-file code)

---
+++ patchBotClass.js
@@ -0,0 +1,16 @@
+'use strict';
+let dsTemplates = require('./lib/discord-templates');
+
+
+
+/**
+ * 
+ * 
+ * @class PatchBot
+ */
+class PatchBot {
+
+    constructor(discordToken, ) {
+        this.templates = dsTemplates
+    }
+}
