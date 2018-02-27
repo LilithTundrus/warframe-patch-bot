76de3b3c4963434c7c000fda29a1cb522a247d98
Lilith Tundrus <echofox123@gmail.com>
2018-02-26 19:09:27 -0500

    Adding basic features to the class and figuring stuff out

--- patchBotClass.js
+++ patchBotClass.js
@@ -1,16 +1,39 @@
 'use strict';
-let dsTemplates = require('./lib/discord-templates');
+const config = require('./config/config.js');                       // conifg/auth data
+const ver = config.botVersion;
+const dsTemplates = require('./lib/discord-templates');             // Set of Discord embed tempaltes
+const Discord = require('discord.io');                              // discord API wrapper
 
 
 
 /**
- * 
+ * This allows for us to multi-instance the bot
  * 
  * @class PatchBot
  */
 class PatchBot {
+    /**
+     * Creates an instance of PatchBot.
+     * @param {String} discordToken 
+     * @param {Object} [options]
+     * @param {Number} [options.shardID]
+     * @param {Boolean} [options.autorun]
+     * @memberof PatchBot
+     */
+    constructor(discordToken, {
+        shardID = 0,
+        autorun = true,
+    }) {
+        this.templates = dsTemplates;
 
-    constructor(discordToken, ) {
-        this.templates = dsTemplates
+        this.client = new Discord.Client({                                      // Initialize Discord Bot with token
+            token: discordToken,
+            autorun: autorun,
+            shard: shardID
+        });
+
     }
+
 }
+
+module.exports = PatchBot;
