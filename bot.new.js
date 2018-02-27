3751361a27faf7c2d0fdad6f994781cc21e12e25
Lilith Tundrus <echofox123@gmail.com>
2018-02-26 22:04:58 -0500

    working towards using the class to do everything we did before

--- bot.new.js
+++ bot.new.js
@@ -1,7 +1,6 @@
 'use strict';                                                       // more stringent error reporting for small things
 const config = require('./config/config.js');                       // conifg/auth data
 const ver = config.botVersion;
-let Discord = require('discord.io');                                // discord API wrapper
 let fs = require('fs');
 let os = require('os');                                             // os info lib built into node
 let Logger = require('./lib/loggerClass');                          // Custom (basic) logger solution
@@ -11,6 +10,27 @@ const logger = new Logger;
 let commonLib = require('./lib/common');
 let PatchBot = require('./patchBotClass');
 let controller = require('./lib/storageController');
-
+// Create a new bot, using defualt options
 let patchBot = new PatchBot(config.token);
 
+
+logger.debug('Attempting to connect to Discord...');
+patchBot.client.on('ready', function (evt) {                        // do some logging and start ensure bot is running
+    logger.info('Connected to Discord', `Logged in as: ${patchBot.client.username} ID: (${patchBot.client.id})`);
+    patchBot.client.setPresence({                                   // make the bot 'play' soemthing
+        idle_since: null,
+        game: { name: 'Debug mode' }
+    });
+    patchBot.initScheduler();
+});
+
+patchBot.client.on('disconnect', function (evt) {
+    logger.warn(`Client DISCONNECTED at ${new Date().toTimeString()}`);
+    logger.debug('Attempting reconnect...');
+    patchBot.client.connect();
+    if (patchBot.client.connected) {
+        logger.info('Reconnected to Discord!');
+    } else {
+        logger.error(new Error('Reconnect failed'));
+    }
+});
